6e38f071524f5b27c550b5691c4e22fca2ff41a9
Sarvanan N <nsaravanan1204@gmail.com>
2023-08-23 14:37:07 +0530

    Spring MVC Boilerplate

---
+++ src/main/java/com/example/Conferencespring/ConferencespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Conferencespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencespringApplication.class, args);
+	}
+
+}
