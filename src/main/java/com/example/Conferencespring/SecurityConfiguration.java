ebec4c49f6117100976240483bfc0fdc5f7420dc
Sarvanan N <nsaravanan1204@gmail.com>
2023-09-05 12:28:46 +0530

    WebSecurityConfigurere Adapter added

---
+++ src/main/java/com/example/Conferencespring/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.Conferencespring;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+protected void Configure(@NotNull AuthenticationManagerBuilder auth)throws Exception{
+auth.inMemoryAuthentication()
+        .withUser("saro")
+        .password("nallakamu")
+        .roles("USER")
+        .and()
+        .withUser("mohan")
+        .password("govindarajan")
+        .roles("ADMIN");
+}
+}
