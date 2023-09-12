76f5f8a0452014450069b6bbed087b5e04e5a796
Sarvanan N <nsaravanan1204@gmail.com>
2023-09-12 10:39:23 +0530

    userDetailsService method is added and configuration is completed

--- src/main/java/com/example/Conferencespring/SecurityConfiguration.java
+++ src/main/java/com/example/Conferencespring/SecurityConfiguration.java
@@ -1,33 +1,61 @@
 package com.example.Conferencespring;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.Access;
 import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("saro")
-                .password("nallakamu")
-                .roles("USER")
-                .and()
-                .withUser("mohan")
-                .password("govindarajan")
-                .roles("ADMIN");
-    };
+        auth.userDetailsService(userDetailsService);
+    }
+
+
+    //    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("saro")
+//                .password("nallakamu")
+//                .roles("USER")
+//                .and()
+//                .withUser("mohan")
+//                .password("govindarajan")
+//                .roles("ADMIN");
+//    };
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser(
+//                        User.withUsername("user")
+//                                .password("pass")
+//                                .roles("USER")
+//                )
+//                .withUser(
+//                        User.withUsername("admin")
+//                                .password("pass")
+//                                .roles("ADMIN")
+//                );
+//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
---
+++ src/main/java/com/example/Conferencespring/model/MyUserDetails.java
@@ -0,0 +1,57 @@
+package com.example.Conferencespring.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+
+    private String UserName;
+
+    public MyUserDetails(String UserName){
+        this.UserName = UserName;
+
+    }
+
+    public MyUserDetails(){
+
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return UserName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
